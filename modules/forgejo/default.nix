f5029914661955ddde5d17cc74c72612a062df5f
Niklas Tasler <niklastasler@gmail.com>
2026-01-17 10:32:38 +0100

    feat: admin provisioning

--- modules/forgejo/default.nix
+++ modules/forgejo/default.nix
@@ -18,6 +18,71 @@ in {
 
   options.nps.stacks.${name} = {
     enable = lib.mkEnableOption name;
+    lfsJwtSecretFile = lib.mkOption {
+      type = lib.types.path;
+      description = ''
+        Path to a file containing the LFS JWT secret.
+        Can be generated using `forgejo generate secret LFS_JWT_SECRET`.
+
+        See <https://forgejo.org/docs/next/admin/config-cheat-sheet/#server-server>
+      '';
+    };
+    secretKeyFile = lib.mkOption {
+      type = lib.types.path;
+      description = ''
+        Path to a file containing the global secret key.
+        Can be generated using `forgejo generate secret SECRET_KEY`.
+
+        See <https://forgejo.org/docs/latest/admin/config-cheat-sheet/#security-security>
+      '';
+    };
+    internalTokenFile = lib.mkOption {
+      type = lib.types.path;
+      description = ''
+        Path to a file containing the internal token.
+        Can be generated using `forgejo generate secret INTERNAL_TOKEN`.
+
+        See <https://forgejo.org/docs/latest/admin/config-cheat-sheet/#security-security>
+      '';
+    };
+    jwtSecretFile = lib.mkOption {
+      type = lib.types.path;
+      description = ''
+        Path to a file containing the OAuth2 jwt secret. This is needed, even if OAuth2 is not used.
+        See <https://codeberg.org/forgejo/forgejo/issues/4570> for more information.
+
+        Can be generated using `forgejo generate secret JWT_SECRET`.
+
+        See <https://forgejo.org/docs/latest/admin/config-cheat-sheet/#oauth2-oauth2>
+      '';
+    };
+    adminProvisioning = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = ''
+          Whether to automatically create an admin user on the first run.
+          If set to false, an admin user can be manually created using the `forgejo` cli.
+
+          See <https://forgejo.org/docs/next/admin/command-line/#admin-user-create>
+        '';
+      };
+      username = lib.mkOption {
+        type = lib.types.addCheck lib.types.str (s: s != "admin");
+        default = "forgejo";
+        description = "Username for the admin user. Cannot be `admin` as that name is reserved.";
+      };
+      email = lib.mkOption {
+        type = lib.types.str;
+        description = "Email address for the admin user ";
+      };
+      passwordFile = lib.mkOption {
+        type = lib.types.path;
+        default = null;
+        description = "Path to a file containing the admin password";
+      };
+    };
+
     settings = lib.mkOption {
       type = lib.types.nullOr ini.type;
       default = null;
@@ -33,13 +98,62 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
+    nps.stacks.${name}.settings = lib.mkMerge [
+      (import ./settings.nix config)
+      {
+        server.FS_JWT_SECRET = "{{ file.Read `${cfg.lfsJwtSecretFile}` }}";
+        security.SECRET_KEY = "{{ file.Read `${cfg.secretKeyFile}` }}";
+        security.INTERNAL_TOKEN = "{{ file.Read `${cfg.internalTokenFile}` }}";
+        oauth2.JWT_SECRET = "{{ file.Read `${cfg.jwtSecretFile}` }}";
+      }
+    ];
+
     services.podman.containers.${name} = {
       image = "codeberg.org/forgejo/forgejo:13";
-      volumes =
+      volumes = [
+        "${storage}/data:/data"
+      ];
+
+      extraConfig.Container = {
+        Notify = "healthy";
+        HealthCmd = "curl -s -f http://localhost:3000 || exit 1";
+        HealthInterval = "10s";
+        HealthTimeout = "10s";
+        HealthRetries = 5;
+        HealthStartPeriod = "5s";
+      };
+      extraConfig.Service.ExecStartPost =
+        lib.mkIf (cfg.adminProvisioning.enable)
         [
-          "${storage}/data:/data"
-        ]
-        ++ lib.optional (cfg.settings != null) "${cfg.settings}:/data/gitea/conf/app.ini";
+          (
+            lib.getExe (
+              pkgs.writeShellScriptBin "forgejo-admin-user-create"
+              (
+                lib.concatStringsSep
+                " "
+                [
+                  "${lib.getExe config.nps.package} exec -u git"
+                  "forgejo forgejo -c /data/gitea/conf/app.ini admin user create"
+                  "--username ${cfg.adminProvisioning.username}"
+                  "--email ${cfg.adminProvisioning.email}"
+                  "--password $(cat ${cfg.adminProvisioning.passwordFile})"
+                  "|| exit 0"
+                ]
+              )
+            )
+          )
+        ];
+
+      # Use template mount instead of "_URI" settings, as app.ini has to be writable anyways
+      templateMount = lib.optional (cfg.settings != null) {
+        templatePath = cfg.settings;
+        destPath = "/data/gitea/conf/app.ini";
+        chown = {
+          user = "1000";
+          group = "1000";
+        };
+      };
+
       ports = ["222:22"];
 
       port = 3000;
